a04caefe8297c803e85384953e7b0de26fc13251
Mark <markbassily2575@gmail.com>
2023-01-02 23:09:48 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+
+
+
+
+prediction = 
+current = 
+if (prediction > current):
+    print(f"{((prediction-current)/current)}  percent increase in the next 30 days.")
+else:
+    print(f"{((prediction-current)/current)}  percent decrease in the next 30 days.")
