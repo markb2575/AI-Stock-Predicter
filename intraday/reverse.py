63ad1afb3f3ea5d9d65ab1bfbb8ad18b3daa9337
markb2575 <81063978+markb2575@users.noreply.github.com>
2023-01-22 11:34:53 -0500

    Delete intraday directory

--- intraday/reverse.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open("intraday/data/SPY.csv") as fr, open("intraday/data/NEW_SPY.csv","w", newline="") as fw:
-    cr = csv.reader(fr,delimiter=",")
-    cw = csv.writer(fw,delimiter=",")
-    cw.writerow(next(cr))  # write title as-is
-    cw.writerows(reversed(list(cr)))
